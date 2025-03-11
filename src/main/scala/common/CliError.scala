87f7240f763c9277dc327b88695fb0f33b14f2bf
flupec <flo0pyyy1@gmail.com>
2025-03-11 13:02:07 +0400

    Format CliError

--- src/main/scala/common/CliError.scala
+++ src/main/scala/common/CliError.scala
@@ -2,8 +2,6 @@ package common
 
 enum CliError extends Error:
   case MissedMandatoryArg(argKey: String)
-  
+
   override def reason: String = this match
     case MissedMandatoryArg(argKey) => s"$argKey is mandatory option"
-    
-  
